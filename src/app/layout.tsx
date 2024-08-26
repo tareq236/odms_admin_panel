b86192f2ecebe55aa2d02a99feb7a6bcd7ea7cac
Jahidul Islam <jahidul.islam.sec@gmail.com>
2024-08-26 14:28:23 +0600

    feat: add font family with feature settings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,8 @@
 import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import ProgressProvider from "@/contexts/ProgressProvider";
 
-const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,7 +19,6 @@ export default function RootLayout({
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable,
         )}
       >
         <ProgressProvider>{children}</ProgressProvider>
