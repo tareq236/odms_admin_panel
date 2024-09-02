abf6c70002ef98c7f64543496db695ee0e801d09
Jahidul Islam <jahidul.islam.sec@gmail.com>
2024-09-02 16:42:02 +0600

    bug: home page card link

--- src/components/home/Card.tsx
+++ src/components/home/Card.tsx
@@ -11,15 +11,13 @@ function Card({
   title: string;
 }) {
   return (
-    <article className="grow cursor-pointer bg-white px-5 py-3 hover:bg-primary/90 transition-all duration-200 group border rounded-lg md:max-w-[16rem]">
-      <Link href={href} className="flex items-center gap-3">
+      <Link href={href} className="flex items-center gap-3 grow cursor-pointer bg-white px-5 py-3 hover:bg-primary/90 transition-all duration-200 group border rounded-lg md:max-w-[16rem]">
         {/* icon */}
         <div className="icon  p-2 w-fit rounded text-primary group-hover:text-white">{icon}</div>
 
         {/* title */}
         <h4 className=" group-hover:text-white">{title}</h4>
       </Link>
-    </article>
   );
 }
 
