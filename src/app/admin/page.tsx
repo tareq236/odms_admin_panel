12553bd14f36e3766353e4e4ae42c004bf984a9f
Jahidul Islam <jahidul.islam.sec@gmail.com>
2024-09-02 16:04:37 +0600

    feat: adding stat in admin home

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,21 +2,41 @@ import { ChartSection } from "@/containers/home/ChartSections";
 import React from "react";
 import db from "../../../db/db";
 import { Prisma } from "@prisma/client";
+import { format } from "date-fns";
+
+export type CartData = {
+  day: Date;
+  total_attendance: bigint;
+};
 
 export default async function Home() {
-  const currentDate = new Date('2024-08-26');
+  const date = new Date();
+  const currentDate = format(new Date(), 'yyyy-MM-dd');
+  const prevMonth = format(new Date(date.getFullYear(), date.getMonth()-1, date.getDate()),'yyyy-MM-dd');
+  let count = 0
+  let data:CartData[] | unknown;
 
-  // const data = await db.$queryRaw(
-  //   Prisma.sql`
-  //   SELECT DATE_
-  //   `
-  // )
+  try {
+    [data, count] = await Promise.all([
+      db.$queryRaw(
+        Prisma.sql`
+    SELECT CONVERT_TZ(start_date_time, '+00:00', '+06:00') as day, COUNT(sap_id) as total_attendance
+    FROM rdl_attendance 
+    WHERE start_date_time > ${prevMonth} AND start_date_time < ${currentDate} 
+    GROUP BY CAST(CONVERT_TZ(start_date_time, '+00:00', '+06:00') as DATE) 
+    `,
+      ),
+      db.rdl_user_list.count(),
 
-  // console.log(data);
+    ]);
+  } catch (error) {
+    data = [];
+  }
+
 
   return (
     <div className="">
-      <ChartSection />
+      <ChartSection data={data as CartData[]} count={count} />
     </div>
   );
 }
