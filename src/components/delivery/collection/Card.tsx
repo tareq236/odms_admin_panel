1bff99895650f4ad5e4d14ad4879120390033709
Jahidul Islam <jahidul.islam.sec@gmail.com>
2024-09-12 17:10:40 +0600

    feat: adding active color of card and inactive status if selected

--- src/components/delivery/collection/Card.tsx
+++ src/components/delivery/collection/Card.tsx
@@ -1,43 +1,69 @@
-'use client'
+"use client";
 
-import { formatNumber } from '@/lib/formatters'
-import { useRouter } from 'next-nprogress-bar'
-import { usePathname, useSearchParams } from 'next/navigation'
-import React from 'react'
+import { formatNumber } from "@/lib/formatters";
+import { useRouter } from "next-nprogress-bar";
+import { usePathname, useSearchParams } from "next/navigation";
+import React from "react";
 
 type CardProps = {
-    icon: React.ReactNode
-    name: string
-    stats: number
-    paramString:string
-}
+  icon: React.ReactNode;
+  name: string;
+  stats: number;
+  paramString: string;
+};
 
-function Card({icon, name, stats, paramString}: CardProps) {
-
-  const searchParams = useSearchParams()
-  const router = useRouter()
-  const pathname = usePathname()
-  const params = new URLSearchParams(searchParams)
-
+function Card({ icon, name, stats, paramString }: CardProps) {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+  const params = new URLSearchParams(searchParams);
 
   return (
-    <article className='flex gap-3 items-center border-r border-b px-4 py-2 cursor-pointer'
+    <article
+      className={`flex gap-3 items-center border-r border-b px-4 py-2 cursor-pointer ${
+        searchParams.get("status") === paramString
+          ? "bg-primary border-primary"
+          : ""
+      }`}
       onClick={() => {
-        params.set('status', paramString)
-        params.delete('p')
-        router.push(pathname + '?' + params.toString())
+        if (searchParams.get("status") === paramString) {
+          params.delete("p");
+          params.delete("status");
+        } else {
+          params.set("status", paramString);
+          params.delete("p");
+        }
+        router.push(pathname + "?" + params.toString());
       }}
     >
-        <div className="icon bg-blue-50 p-2 rounded text-primary">
-            {icon}
-        </div>
+      <div
+        className={`icon bg-blue-50 p-2 rounded text-primary ${
+          searchParams.get("status") === paramString ? "bg-white" : ""
+        }`}
+      >
+        {icon}
+      </div>
 
-        <div className="description flex flex-col gap-0.5">
-            <h4 className='text-muted-foreground text-xs'>{name}</h4>
-            <h5 className='text-2xl'>{formatNumber(stats)}</h5>
-        </div>
+      <div className="description flex flex-col gap-0.5">
+        <h4
+          className={`${
+            searchParams.get("status") === paramString
+              ? "text-white"
+              : "text-muted-foreground"
+          } text-xs`}
+        >
+          {name}
+        </h4>
+        <h5
+          className={`text-2xl ${
+            searchParams.get("status") === paramString ? "text-white" : ""
+          }`}
+        >
+          {formatNumber(stats)}
+        </h5>
+      </div>
     </article>
-  )
+  );
 }
 
-export default Card
+export default Card;
