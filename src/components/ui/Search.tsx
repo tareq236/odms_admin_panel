6ac21c350050d92bd9dedd7d2b0d50a8ce767af9
Jahidul Islam <jahidul.islam.sec@gmail.com>
2024-09-12 12:03:24 +0600

    feat: adding search bar button

--- src/components/ui/Search.tsx
+++ src/components/ui/Search.tsx
@@ -7,37 +7,38 @@ import { SearchIcon } from "lucide-react";
 import { useRouter } from "next-nprogress-bar";
 import { usePathname, useSearchParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
+import { Button } from "./button";
 
 function Search({ placeholder = "Search..." }: { placeholder?: string }) {
   const searchParams = useSearchParams();
-  const [search, setSearch] = useState(searchParams.get('q') || "");
-  const debounceValue = useDebounce(search);
+  const [search, setSearch] = useState(searchParams.get("q") || "");
+  // const debounceValue = useDebounce(search);
 
   const router = useRouter();
   const pathname = usePathname();
 
   const params = new URLSearchParams(searchParams);
 
-  useEffect(() => {
-    if (search) {
-      params.set("q", debounceValue);
-      params.delete("p");
-    } else {
-      params.delete("q");
-      params.delete("p");
-    }
-    router.push(pathname + "?" + params.toString());
-  }, [debounceValue]);
+  // useEffect(() => {
+  //   if (search) {
+  //     params.set("q", debounceValue);
+  //     params.delete("p");
+  //   } else {
+  //     params.delete("q");
+  //     params.delete("p");
+  //   }
+  //   router.push(pathname + "?" + params.toString());
+  // }, [debounceValue]);
 
   return (
-    <div className="relative">
+    <div className="relative min-w-[5rem]">
       <Input
         type="search"
         name="search"
         id="search"
         value={search}
         onChange={(e) => setSearch(e.target.value)}
-        className="pl-8 min-w-[5rem]"
+        className="pl-8 pr-12 w-full"
         placeholder={placeholder}
       />
       <Label
@@ -46,6 +47,22 @@ function Search({ placeholder = "Search..." }: { placeholder?: string }) {
       >
         <SearchIcon className="size-4" />
       </Label>
+      <Button
+        size={"icon"}
+        className="absolute right-0 top-0 rounded-l-none"
+        onClick={() => {
+          if (search) {
+            params.set("q", search);
+            params.delete("p");
+          } else {
+            params.delete("q");
+            params.delete("p");
+          }
+          router.push(pathname + "?" + params.toString());
+        }}
+      >
+        <SearchIcon className="size-4" />
+      </Button>
     </div>
   );
 }
