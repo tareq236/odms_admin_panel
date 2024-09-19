7752be4ca0b4684a7a6f562864f5f1121caba504
Jahidul Islam <jahidul.islam.sec@gmail.com>
2024-09-19 10:46:30 +0600

    fix: issue in login redirect

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,17 +1,8 @@
 import LoginForm from "@/components/login/LoginForm";
-import { getUser } from "@/lib/dal";
-import { decrypt } from "@/lib/session";
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
 import React from "react";
 
 export default async function LoginPage() {
-  const user = await getUser()
-
-  if (user != null) {
-    redirect("/admin");
-  }
-  
+    
   return (
     <>
       <LoginForm />
