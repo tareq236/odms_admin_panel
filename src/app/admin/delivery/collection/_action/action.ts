f544f5d0f3e3bc3032c2e8c89682294d2cd93dd6
Jahidul Islam <jahidul.islam.sec@gmail.com>
2024-09-26 11:10:34 +0600

    bug: delivery return data query

--- src/app/admin/delivery/collection/_action/action.ts
+++ src/app/admin/delivery/collection/_action/action.ts
@@ -22,7 +22,7 @@ export const getDeliveryCollection = async ({
           db.$queryRaw`
             SELECT a.billing_date, a.billing_doc_no, 
             b.delivery_status, b.cash_collection_status,
-            (b.cash_collection) cash_collection, sum(b.due_amount) due_amount, sum(c.net_val) + sum(c.vat) as net_val,
+            (b.cash_collection) cash_collection, (b.due_amount) due_amount, sum(c.net_val) + sum(c.vat) as net_val,
             c.partner, b.id,
             d.name1
             FROM rdl_delivery_info_sap as a
@@ -56,7 +56,7 @@ export const getDeliveryCollection = async ({
           db.$queryRaw`
             SELECT a.billing_date, a.billing_doc_no, 
             b.delivery_status, b.cash_collection_status,
-            (b.cash_collection) cash_collection, sum(b.due_amount) due_amount, sum(c.net_val) + sum(c.vat) as net_val,
+            (b.cash_collection) cash_collection, (b.due_amount) due_amount, sum(c.net_val) + sum(c.vat) as net_val,
             c.partner, b.id,
             d.name1
             FROM rdl_delivery_info_sap as a
@@ -90,7 +90,7 @@ export const getDeliveryCollection = async ({
           db.$queryRaw`
             SELECT a.billing_date, a.billing_doc_no, 
             b.delivery_status, b.cash_collection_status,
-            (b.cash_collection) cash_collection, sum(b.due_amount) due_amount, sum(c.net_val) + sum(c.vat) as net_val,
+            (b.cash_collection) cash_collection, (b.due_amount) due_amount, sum(c.net_val) + sum(c.vat) as net_val,
             c.partner, b.id,
             d.name1
             FROM rdl_delivery_info_sap as a
@@ -124,7 +124,7 @@ export const getDeliveryCollection = async ({
           db.$queryRaw`
             SELECT a.billing_date, a.billing_doc_no, 
             b.delivery_status, b.cash_collection_status,
-            (b.cash_collection) cash_collection, sum(b.due_amount) due_amount, sum(c.net_val) + sum(c.vat) as net_val,
+            (b.cash_collection) cash_collection, (b.due_amount) due_amount, sum(c.net_val) + sum(c.vat) as net_val,
             c.partner, b.id,
             d.name1
             FROM rdl_delivery_info_sap as a
@@ -156,35 +156,35 @@ export const getDeliveryCollection = async ({
       } else if (searchParams.status == "r") {
         [data, count] = await Promise.all([
           db.$queryRaw`
-            SELECT a.billing_date, a.billing_doc_no, 
-            b.delivery_status, b.cash_collection_status,
-            (b.cash_collection) cash_collection, sum(b.due_amount) due_amount, sum(c.net_val) + sum(c.vat) as net_val,
-            c.partner, b.id,
-            d.name1
-            FROM rdl_delivery_info_sap as a
-            LEFT JOIN rdl_delivery as b ON a.billing_doc_no = b.billing_doc_no
-            INNER JOIN rpl_sales_info_sap as c ON a.billing_doc_no = c.billing_doc_no
-            INNER JOIN rpl_customer as d ON c.partner=d.partner
-            WHERE a.billing_date = ${
+            select rd.id, rd.billing_date, rd.billing_doc_no, 
+            rd.delivery_status, rd.cash_collection_status,
+            rd.cash_collection, rd.due_amount, rd.net_val, 
+            SUM(rds.return_quantity) return_quantity,
+            SUM(rds.delivery_quantity) delivery_quantity,
+            SUM(rds.quantity) quantity,
+            rd.partner, 
+            rc.name1
+            FROM rdl_delivery rd
+            INNER JOIN rdl_delivery_list rds ON rds.delivery_id = rd.id
+            INNER JOIN rpl_customer rc ON rc.partner = rd.partner
+            WHERE rd.billing_date = ${
               searchParams.start ? `${searchParams.start}` : `${formateDateDB(new Date())}`
             } 
-            AND a.da_code = ${Number(searchParams.q) || 0} 
-            AND e.return_quantity IS NOT NULL
-            AND e.return_quantity != 0
-            GROUP BY a.billing_doc_no
+            AND rd.da_code = ${Number(searchParams.q) || 0} 
+            AND rds.return_quantity > 0
+            GROUP BY rd.billing_doc_no
             LIMIT ${(Number(searchParams.p || 1) - 1) * limit}, ${limit}
             `,
           db.$queryRaw`
-            select count(*) over() as total from rdl_delivery_info_sap as a
-            LEFT JOIN rdl_delivery as b ON a.billing_doc_no = b.billing_doc_no
-            LEFT JOIN rdl_delivery_list as e ON b.id = e.delivery_id
-            WHERE a.billing_date = ${
+            select count(*) over() as total FROM rdl_delivery rd
+            INNER JOIN rdl_delivery_list rds ON rds.delivery_id = rd.id
+            INNER JOIN rpl_customer rc ON rc.partner = rd.partner
+            WHERE rd.billing_date = ${
               searchParams.start ? `${searchParams.start}` : `${formateDateDB(new Date())}`
             } 
-            AND a.da_code = ${Number(searchParams.q) || 0}
-            AND e.return_quantity IS NOT NULL
-            AND e.return_quantity != 0
-            GROUP BY a.billing_doc_no
+            AND rd.da_code = ${Number(searchParams.q) || 0}
+            AND rds.return_quantity > 0
+            GROUP BY rd.billing_doc_no
             limit 1
           `,
         ]);
@@ -193,7 +193,7 @@ export const getDeliveryCollection = async ({
             db.$queryRaw`
                 SELECT a.billing_date, a.billing_doc_no, 
                 b.delivery_status, b.cash_collection_status,
-                (b.cash_collection) cash_collection, sum(b.due_amount) due_amount, sum(c.net_val) + sum(c.vat) as net_val,
+                (b.cash_collection) cash_collection, (b.due_amount) due_amount, sum(c.net_val) + sum(c.vat) as net_val,
                 c.partner, b.id,
                 d.name1
                 FROM rdl_delivery_info_sap as a
@@ -223,7 +223,7 @@ export const getDeliveryCollection = async ({
             db.$queryRaw`
                 SELECT a.billing_date, a.billing_doc_no, 
                 b.delivery_status, b.cash_collection_status,
-                (b.cash_collection) cash_collection, sum(b.due_amount) due_amount, sum(c.net_val) + sum(c.vat) as net_val,
+                (b.cash_collection) cash_collection, (b.due_amount) due_amount, sum(c.net_val) + sum(c.vat) as net_val,
                 c.partner, b.id,
                 d.name1
                 FROM rdl_delivery_info_sap as a
