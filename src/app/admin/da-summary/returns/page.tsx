e0c3e24417b21ec28c75f0888c93e1097d77fb3d
Jahidul Islam <jahidul.islam.sec@gmail.com>
2024-10-21 13:03:08 +0600

    feat: adding return product by billing docs

--- src/app/admin/da-summary/returns/page.tsx
+++ src/app/admin/da-summary/returns/page.tsx
@@ -1,9 +1,29 @@
 import ReturnsTable from "@/components/da-summary/returns/ReturnsTable";
 import React from "react";
 import db from "../../../../../db/db";
-import {formateDateDB} from '@/lib/formatters';
+import { formateDateDB } from "@/lib/formatters";
 
-async function ReturnSummaryPage({searchParams}: {searchParams: {q: string, start: string}}) {
+async function ReturnSummaryPage({
+  searchParams,
+}: {
+  searchParams: { q: string; start: string };
+}) {
+  let billingDocs: (unknown | any)[];
+  try {
+    billingDocs = await db.$queryRaw`
+      select rd.billing_doc_no FROM rdl_delivery rd
+      INNER JOIN rdl_delivery_list rdl ON rdl.delivery_id = rd.id
+      WHERE rd.da_code = ${Number(searchParams.q) || 0} and rd.billing_date = ${
+      searchParams.start
+        ? `${searchParams.start}`
+        : `${formateDateDB(new Date())}`
+    } and rdl.return_quantity > 0
+      GROUP BY rd.billing_doc_no
+    `;
+  } catch (error) {
+    billingDocs = [];
+  }
+
   let returnProducts;
   try {
     returnProducts = await db.$queryRaw`
@@ -13,20 +33,60 @@ async function ReturnSummaryPage({searchParams}: {searchParams: {q: string, star
     INNER JOIN rpl_sales_info_sap rsis ON rsis.billing_doc_no = rd.billing_doc_no
     INNER JOIN rpl_material rm ON rm.matnr = rsis.matnr
     WHERE rd.da_code = ${Number(searchParams.q) || 0} and rd.billing_date = ${
-          searchParams.start
-            ? `${searchParams.start}`
-            : `${formateDateDB(new Date())}`
-        }  and rdl.return_quantity > 0
+      searchParams.start
+        ? `${searchParams.start}`
+        : `${formateDateDB(new Date())}`
+    }  and rdl.return_quantity > 0
     GROUP BY rsis.matnr
     ORDER BY rm.material_name
   `;
   } catch (error) {
-    returnProducts = []
+    returnProducts = [];
+  }
+
+  // single billings
+  let singleBills: any[] = [];
+  try {
+    for (let i = 0; i < billingDocs.length; i++) {
+      let data = await db.$queryRaw`
+        SELECT rsis.matnr, rm.material_name, (rdl.return_quantity) quantity, rd.billing_doc_no 
+        FROM rdl_delivery rd
+        INNER JOIN rdl_delivery_list rdl ON rdl.delivery_id = rd.id
+        INNER JOIN rpl_sales_info_sap rsis ON rsis.billing_doc_no = rd.billing_doc_no
+        INNER JOIN rpl_material rm ON rm.matnr = rsis.matnr
+        WHERE rd.da_code = ${
+          Number(searchParams.q) || 0
+        } and rd.billing_date = ${
+        searchParams.start
+          ? `${searchParams.start}`
+          : `${formateDateDB(new Date())}`
+      } 
+        and rdl.return_quantity > 0 AND rd.billing_doc_no = ${
+          billingDocs[i].billing_doc_no
+        }
+      `;
+
+      singleBills.push(data)
+    }
+  } catch (error) {
+    singleBills = []
   }
 
   return (
-    <div className="">
-      <ReturnsTable show={true} title="Overview" returnProducts={returnProducts as any[]} />
+    <div className="flex flex-col gap-5">
+      <ReturnsTable
+        show={true}
+        title="Overview"
+        returnProducts={returnProducts as any[]}
+      />
+
+      {
+        singleBills && 
+        singleBills.map(item => (
+          <ReturnsTable key={item} title={`Billing No - ${item[0].billing_doc_no}`}
+          returnProducts={item} />
+        ))
+      }
     </div>
   );
 }
