1507becc124019e92f0543f5cc0f3fef0a5774ee
Jahidul Islam <jahidul.islam.sec@gmail.com>
2024-10-22 13:07:41 +0600

    chore: action of return product list in da summary

--- src/app/admin/da-summary/returns/_actions/action.ts
+++ src/app/admin/da-summary/returns/_actions/action.ts
@@ -6,23 +6,7 @@ export const getReturnData = async (searchParams: {
   start: string;
 }) => {
 
-  let partners: (unknown | any)[];
-
-  try {
-    partners = await db.$queryRaw`
-    SELECT DISTINCT rl.partner
-    FROM rdl_return_list rl
-    where rl.da_code = ${Number(searchParams.q) || 0} AND rl.billing_date=${
-      searchParams.start
-        ? `${searchParams.start}`
-        : `${formateDateDB(new Date())}`
-    }
-    `;
-  } catch (error) {
-    partners = [];
-  }
-
-  let returnProducts;
+  let returnProducts: (unknown | any)[];
   try {
     returnProducts = await db.$queryRaw`
       select  rl.matnr, rm.material_name, rl.batch, 
@@ -46,7 +30,7 @@ export const getReturnData = async (searchParams: {
   // single billings
   let singleBills: any[] = [];
   try {
-    for (let i = 0; i < partners.length; i++) {
+    for (let i = 0; i < returnProducts.length; i++) {
       let data = await db.$queryRaw`
         select  rl.matnr, rm.material_name, rl.batch, 
         SUM(rl.return_quantity) quantity, SUM(rl.return_net_val) net_val, 
@@ -61,7 +45,7 @@ export const getReturnData = async (searchParams: {
           ? `${searchParams.start}`
           : `${formateDateDB(new Date())}`
         } 
-        AND rl.partner=${partners[i].partner}
+        AND rl.partner=${returnProducts[i].partner}
         GROUP BY rl.matnr
         ORDER BY rm.material_name
       `;
@@ -74,7 +58,6 @@ export const getReturnData = async (searchParams: {
 
   return {
     singleBills,
-    partners,
     returnProducts,
   };
 };
