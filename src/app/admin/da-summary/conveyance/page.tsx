146e51107f2b34f64151563a5a8bade5fdaf2271
Jahidul Islam <jahidul.islam.sec@gmail.com>
2024-10-23 15:26:32 +0600

    fix(build)!: da summary conveyance page issue

--- src/app/admin/da-summary/conveyance/page.tsx
+++ src/app/admin/da-summary/conveyance/page.tsx
@@ -1,10 +1,7 @@
 import SearchDa from "@/components/constants/SearchDa";
-import ConveyanceTable from "@/components/conveyance/ConveyanceTable";
-import PagePagination from "@/components/ui/PagePagination";
 import React, { Suspense } from "react";
-import { getConveyanceData } from "../../conveyance/_action/action";
 import TableSkeleton from "@/components/ui/TableSkeletion";
-import { DataTable } from "../../conveyance/page";
+import { DataTable } from "../../map/conveyance/page";
 
 export default function DaCoveyancePage({
   searchParams,
