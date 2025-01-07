8f95abc0d4c5b513d03f77e842be52a113e13f81
Jahidul Islam <jahidul.app@gmail.com>
2025-01-07 12:19:17 +0600

    feat: adding depot wise attendance chart

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -4,6 +4,8 @@ import { Prisma } from "@/prisma/generated/client1";
 import { format } from "date-fns";
 import Header from "@/components/home/Header";
 import { ChartSection } from "@/components/home/ChartSections";
+import { getUser } from "@/lib/dal";
+import { redirect } from "next/navigation";
 
 export type CartData = {
   day: Date;
@@ -37,18 +39,42 @@ export default async function Home({
   let count = 0;
   let data: CartData[] | unknown;
 
+  // get auth user info
+  const authUser = await getUser();
+
+  if (!authUser) return redirect("/login");
+
   try {
-    [data, count] = await Promise.all([
-      db.$queryRaw(
-        Prisma.sql`
-        SELECT CAST(start_date_time as DATE)  as day, COUNT(sap_id) as total_attendance
-        FROM rdl_attendance 
-        WHERE start_date_time > ${prevMonth} AND start_date_time < ${currentDate} 
-        GROUP BY CAST(start_date_time as DATE) 
-    `
-      ),
-      db.rdl_user_list.count(),
-    ]);
+    if (authUser.role === "admin") {
+      [data, count] = await Promise.all([
+        db.$queryRaw(
+          Prisma.sql`
+          SELECT CAST(start_date_time as DATE)  as day, COUNT(sap_id) as total_attendance
+          FROM rdl_attendance 
+          WHERE start_date_time > ${prevMonth} AND start_date_time < ${currentDate} 
+          GROUP BY CAST(start_date_time as DATE) 
+      `
+        ),
+        db.rdl_users_list.count(),
+      ]);
+    } else {
+      [data, count] = await Promise.all([
+        db.$queryRaw(
+          Prisma.sql`
+          SELECT CAST(ra.start_date_time as DATE) as day, COUNT(ra.sap_id) as total_attendance
+          FROM rdl_attendance ra
+          LEFT JOIN rdl_users_list ru ON ru.sap_id = ra.sap_id
+          WHERE start_date_time > ${prevMonth} AND start_date_time < ${currentDate} 
+              AND ru.depot_code = ${authUser.depot_code}
+          GROUP BY
+              CAST(ra.start_date_time as DATE)
+      `
+        ),
+        db.rdl_users_list.count({
+          where: { depot_code: authUser.depot_code },
+        }),
+      ]);
+    }
   } catch (error) {
     data = [];
   }
